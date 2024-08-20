8f2e4a3416ff92c27457e78df37ecd6fd7d6d558
Kasey Klimes <kaseyklimes@gmail.com>
2024-08-20 14:40:46 -0400

    Implement Formspree contact form

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/contact', (req, res) => {
-  res.redirect('/thank-you.html');
-});
-
 app.get('*', (req, res) => {
   if (req.path.endsWith('.html')) {
     res.sendFile(path.join(__dirname, 'public', 'html', req.path));
